091e613bd43bcfe294705e0f50f565c4282eb98d
thesamim <thesamim@gmail.com>
2025-06-05 13:07:21 -0500

    Feature: Shiny new tabbed interface for settings Feature: No longer have to click a button to get folder location set.

---
+++ src/ui/settings/constants.svelte.js
@@ -0,0 +1,9 @@
+export const PROVIDER_OPTIONS = { 'ticktick.com': 'TickTick', 'dida365.com': 'Dida365' };
+export const TAGS_BEHAVIOR = { 1: 'AND', 2: 'OR' };
+export const LOG_LEVEL = {
+	'trace': 'trace',
+	'debug': 'debug',
+	'info': 'info',
+	'warn': 'warn',
+	'error': 'error'
+};
