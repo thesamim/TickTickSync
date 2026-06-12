fb36432dcaef3dc4a2ceb5bb58a1950dc5a58846
thesamim <thesamim@gmail.com>
2026-06-12 16:38:31 -0500

    Chore: fix vulnerabilities

--- src/db/dexie.ts
+++ src/db/dexie.ts
@@ -1,4 +1,5 @@
-import Dexie, { Table } from "dexie";
+import Dexie from "dexie";
+import type { Table } from "dexie";
 import type { LocalTask, SyncMeta } from "./schema";
 import type { TaskFileMapping, LocalProject, LocalProjectGroup, LocalFile } from "./schema";
 
